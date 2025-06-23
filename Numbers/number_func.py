ca6e713761023beb01fc318e6b6c0a283a8ada73
Syed Sameer Hussain Shah <syedsameershah0786@gmail.com>
2025-06-24 02:23:22 +0500

    Update number_func.py

--- Numbers/number_func.py
+++ Numbers/number_func.py
@@ -1,3 +1,4 @@
 import math
 round(2.4) # works like nomal roundoff
-print(round(2.4)) # always upper roundoff
+print(round(2.4)) # always upper roundoff If the decimal part is exactly .5, it rounds to the nearest even number (not always rounding up or down).
+
