912c9dc6dbb5643e92cd7d48e6cb7a01bfcf4f98
Syed Sameer Hussain Shah <syedsameershah0786@gmail.com>
2025-06-24 02:30:37 +0500

    Update stringMethods.py

--- String/stringMethods.py
+++ String/stringMethods.py
@@ -1,5 +1,6 @@
 text = "My name is sameer"
-# 01-2-3456-7-89-10-11 12 13 14 15 16
+# 01-2-3456-7-89-10-11 12 13 14 15 16 spaces also count
+
 new_text = text.upper()
 print(new_text)
 print(text.lower())
